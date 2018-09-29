3d29001aa468ee5d1412b81d11b56ffd31ccd5ab
Levente Vig <vig.levente@codeyard.eu>
2018-09-29 10:52:51 +0200

    Initial Commit

---
+++ rft/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  rft
+//
+//  Created by Levente Vig on 2018. 09. 29..
+//  Copyright © 2018. Levente Vig. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
