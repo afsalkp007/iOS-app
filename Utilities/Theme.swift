669475177bc75e08673766c3102b4a3cf5545321
Levente Vig <levivig@gmail.com>
2018-10-19 21:14:10 +0200

    💄 Added some colors to Theme

--- Utilities/Theme.swift
+++ Utilities/Theme.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+import UIKit
+
+class Theme: NSObject {
+	// MARK: Singleton
+	static let shared = Theme()
+	
+	private override init() {
+		super.init()
+	}
+	
+	// MARK: - Colors
+	static let darkBlue = UIColor(0x112F41)
+	static let lightGreen = UIColor(0x4FB99F)
+	static let orange = UIColor(0xF2B134)
+	static let red = UIColor(0xED553B)
+	static let teal = UIColor(0x068587)
+}
