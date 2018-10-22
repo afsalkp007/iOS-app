233269c8b69ab54ecb584c1e0825a1b1b4b70f56
Levente Vig <levivig@gmail.com>
2018-10-22 10:48:26 +0200

    ♻️ Reindent

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -12,89 +12,89 @@ import GoogleSignIn
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
-    
-    var window: UIWindow?
-    var loggedInUser: User? {
-        didSet {
-            do {
-                let encodedData = try NSKeyedArchiver.archivedData(withRootObject: loggedInUser!, requiringSecureCoding: true)
-                defaults.set(encodedData, forKey: Constants.UserDefaultsKeys.loggedInUser)
-            } catch {
-                NSLog("Failed to set defaults key \(Constants.UserDefaultsKeys.loggedInUser)")
-            }
-        }
-    }
-    
-    let defaults = UserDefaults.standard
-    
-    
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
-        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
-        GIDSignIn.sharedInstance().delegate = self
-        
-        if isKeyPresentInUserDefaults(key: Constants.UserDefaultsKeys.loggedInUser) {
-            let decoded = defaults.object(forKey: Constants.UserDefaultsKeys.loggedInUser) as! Data
-            do {
-                try loggedInUser = NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(decoded) as? User
-            } catch {
-                NSLog("Failed to decode user.")
-            }
-        }
-        
-        return true
-    }
-    
-    @available(iOS 9.0, *)
-    func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any])
-        -> Bool {
-            return GIDSignIn.sharedInstance().handle(url, sourceApplication:options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
-                                                     annotation: [:])
-    }
-    
-    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
-        // ...
-        if let error = error {
-            // Login failed
-            NSLog("😢 \(error)")
-            return
-        }
-        
-        guard let authentication = user.authentication else { return }
-        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
-                                                       accessToken: authentication.accessToken)
-        // Login succesful
-        NSLog("👌 \(credential)")
-    }
-    
-    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
-        // Perform any operations when the user disconnects from app here.
-        // ...
-    }
-    
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-    
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-    
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-    
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-    
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-    
-    
+	
+	var window: UIWindow?
+	var loggedInUser: User? {
+		didSet {
+			do {
+				let encodedData = try NSKeyedArchiver.archivedData(withRootObject: loggedInUser!, requiringSecureCoding: true)
+				defaults.set(encodedData, forKey: Constants.UserDefaultsKeys.loggedInUser)
+			} catch {
+				NSLog("Failed to set defaults key \(Constants.UserDefaultsKeys.loggedInUser)")
+			}
+		}
+	}
+	
+	let defaults = UserDefaults.standard
+	
+	
+	
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		FirebaseApp.configure()
+		GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+		GIDSignIn.sharedInstance().delegate = self
+		
+		if isKeyPresentInUserDefaults(key: Constants.UserDefaultsKeys.loggedInUser) {
+			let decoded = defaults.object(forKey: Constants.UserDefaultsKeys.loggedInUser) as! Data
+			do {
+				try loggedInUser = NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(decoded) as? User
+			} catch {
+				NSLog("Failed to decode user.")
+			}
+		}
+		
+		return true
+	}
+	
+	@available(iOS 9.0, *)
+	func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any])
+		-> Bool {
+			return GIDSignIn.sharedInstance().handle(url, sourceApplication:options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+													 annotation: [:])
+	}
+	
+	func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
+		// ...
+		if let error = error {
+			// Login failed
+			NSLog("😢 \(error)")
+			return
+		}
+		
+		guard let authentication = user.authentication else { return }
+		let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
+													   accessToken: authentication.accessToken)
+		// Login succesful
+		NSLog("👌 \(credential)")
+	}
+	
+	func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
+		// Perform any operations when the user disconnects from app here.
+		// ...
+	}
+	
+	func applicationWillResignActive(_ application: UIApplication) {
+		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+		// Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+	}
+	
+	func applicationDidEnterBackground(_ application: UIApplication) {
+		// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+		// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+	}
+	
+	func applicationWillEnterForeground(_ application: UIApplication) {
+		// Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+	}
+	
+	func applicationDidBecomeActive(_ application: UIApplication) {
+		// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+	}
+	
+	func applicationWillTerminate(_ application: UIApplication) {
+		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+	}
+	
+	
 }
 
